0692c933511c8f9c43a570a791558c6027d115f2
AdamI19 <broadscars@gmail.com>
2021-07-19 14:35:15 -0600

    finished task 1 and 2

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   Study the code for counter1 and counter2, then answer the questions below.
   
-  1. What is the difference between counter1 and counter2?
+  1. What is the difference between counter1 and counter2? counter 1 has a closure.
   
-  2. Which of the two uses a closure? How can you tell?
+  2. Which of the two uses a closure? How can you tell? counter 1 because counter1 is reaching up to the parent level.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better?  counter 1 would be preferable when using a scope only in the function. counter 2 code would be preferable when you would need to refference count as a global scope for later use.
 */
 
 // counter1 code
@@ -62,9 +62,13 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(scorecb){
+    return Math.floor(Math.random() * Math.floor(3));
+  
 }
+
+ 
+console.log('task 2', inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,17 +85,38 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+
+
+function finalScore(innings, number){
+  const currentScore = [];
+  const theScore = {
+   home: 0,
+   away: 0,
+  }
+  for(let i = 0; i < 10; i++){
+    const currentScore = number(innings);
+    inning() = homeScore + currentScore.Home;
+    inning() = awayScore + currentScore.Away;
+    totalGame.push (`Inning ${i + 1}: Away ${homeScore} - Home ${awayScore}`);
+  }
+  return currentScore;
 }
+console.log(finalScore(inning, 9));
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore() {
+
+
+
+
+
+
+
 }
 
 
